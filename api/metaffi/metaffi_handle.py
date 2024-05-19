9e8e7638fda5c935214bbff568997b517f7c974c
TCs <green.fuzer@gmail.com>
2024-05-19 14:09:28 +0300

    update metaffi_handle python class

--- api/metaffi/metaffi_handle.py
+++ api/metaffi/metaffi_handle.py
@@ -2,15 +2,12 @@ import ctypes
 
 ReleaserFuncType = ctypes.CFUNCTYPE(None, ctypes.c_void_p)
 
-
 class metaffi_handle:
-	def __init__(self, h: ctypes.c_void_p, runtime_id: ctypes.c_uint64, releaser: ctypes.c_void_p):
-		self.handle = ctypes.c_void_p(h)
+	def __init__(self, h, runtime_id, releaser):
+		self.handle = h
 		self.runtime_id = runtime_id
 		self.releaser = ctypes.cast(releaser, ReleaserFuncType)
-		
+	
 	def __del__(self):
 		if self.releaser:
-			self.releaser(self.handle)
-		
-	
+			self.releaser(ctypes.c_void_p(self.handle))
