9f025dcf04d6a71a2c2d6168d3549ec7f306c2b0
GreenFuze <6296195+GreenFuze@users.noreply.github.com>
2024-05-27 19:11:19 +0300

    .

--- api/metaffi/__init__.py
+++ api/metaffi/__init__.py
@@ -1,14 +1,14 @@
 """Python MetaFFI API"""
 
-__version__ = "0.0.29"
+__version__ = "0.0.30"
 
-__all__ = ['metaffi', 'metaffi_types', 'metaffi_runtime', 'metaffi_module', 'metaffi_handle', 'metaffi_types', 'xllr_wrapper', 'pycdts_converter', 'metaffi_type_info', 'MetaFFITypes', 'MetaFFIEntity']
+__all__ = ['metaffi', 'metaffi_types', 'metaffi_runtime', 'metaffi_module', 'MetaFFIHandle', 'metaffi_types', 'xllr_wrapper', 'pycdts_converter', 'metaffi_type_info', 'MetaFFITypes', 'MetaFFIEntity']
 
 import metaffi
 from . import metaffi_types
 from . import metaffi_runtime
 from . import metaffi_module
-from .metaffi_handle import metaffi_handle
+from .metaffihandle import MetaFFIHandle
 from . import xllr_wrapper
 from . import pycdts_converter
 from .metaffi_types import metaffi_type_info
