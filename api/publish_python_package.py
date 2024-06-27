619402901b9bde717a41609bfb5c8bca3d7e5c3e
GreenFuze <6296195+GreenFuze@users.noreply.github.com>
2024-06-27 12:05:02 +0300

    .

--- api/publish_python_package.py
+++ api/publish_python_package.py
@@ -2,45 +2,51 @@ import shutil
 import subprocess
 import time
 import os
+from typing import final
 
 # Change directory to the current directory of the __file__
 current_dir = os.getcwd()
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# Move ./tests directory to parent directory
-if os.path.exists('./tests') and (not os.path.exists('../tests') or not os.listdir('../tests')):
-	shutil.rmtree('../tests', ignore_errors=True)
-	shutil.move('./tests', '..')
+try:
+	shutil.move("SConscript_api-python3", '..')
 
-if os.path.exists('./unittest') and (not os.path.exists('../unittest') or not os.listdir('../unittest')):
-	shutil.rmtree('../unittest', ignore_errors=True)
-	shutil.move('./unittest', '..')
+	if os.path.exists('./tests') and (not os.path.exists('../tests') or not os.listdir('../tests')):
+		shutil.rmtree('../tests', ignore_errors=True)
+		shutil.move('./tests', '..')
 
-shutil.move("SConscript_api-python3", '..')
+	if os.path.exists('./unittest') and (not os.path.exists('../unittest') or not os.listdir('../unittest')):
+		shutil.rmtree('../unittest', ignore_errors=True)
+		shutil.move('./unittest', '..')
 
-subprocess.run(['git', 'add', '*'], check=True)
-subprocess.run(['git', 'commit', '-m', '.'], check=True)
+	subprocess.run(['git', 'add', '*'], check=True)
+	subprocess.run(['git', 'commit', '-m', '.'], check=True)
 
-# Publish to pypi
-subprocess.run(['flit', 'publish', '--repository', 'pypi', '--pypirc', os.path.expanduser("~")+'/.pyirc'], check=True)
+	# Publish to pypi
+	subprocess.run(['flit', 'publish', '--repository', 'pypi', '--pypirc', os.path.expanduser("~")+'/.pyirc'], check=True)
 
-# Move back the "tests" directory from parent directory to current directory
-shutil.move('../tests', '.')
-shutil.move('../unittest', '.')
-shutil.move("../SConscript_api-python3", '.')
+	# Move back the "tests" directory from parent directory to current directory
+	shutil.move('../tests', '.')
+	shutil.move('../unittest', '.')
 
-subprocess.run(['git', 'add', '*'], check=True)
-subprocess.run(['git', 'commit', '-m', '.'], check=True)
-subprocess.run(['git', 'push'], check=True)
+	subprocess.run(['git', 'add', '*'], check=True)
+	subprocess.run(['git', 'commit', '-m', '.'], check=True)
+	subprocess.run(['git', 'push'], check=True)
 
-# wait for pypi to update
-print("waiting 5 seconds for pypi to update")
-time.sleep(5)
+	# wait for pypi to update
+	print("waiting 5 seconds for pypi to update")
+	time.sleep(5)
 
-# Update metaffi-api pip package
-subprocess.run(['py', '-m', 'pip', 'install', 'metaffi-api', '--upgrade'], check=True)
+	# Update metaffi-api pip package
+	subprocess.run(['py', '-m', 'pip', 'install', 'metaffi-api', '--upgrade'], check=True)
 
-# Change back to the previous current directory
-os.chdir(current_dir)
+	# Change back to the previous current directory
+	os.chdir(current_dir)
 
-print("done updating package")
+	print("done updating package")
+
+finally:
+	shutil.move("../SConscript_api-python3", '.')
+
+	# Change back to the previous current directory
+	os.chdir(current_dir)
